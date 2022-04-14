0eb87acc2c027bf19527d9f7b1c5ea9d7ebd49d3
Alex <arusskikhqa@gmail.com>
2022-04-14 14:46:56 -0400

    added variables exmlps

---
+++ variables_.js
@@ -0,0 +1,22 @@
+//PascalCase
+// DB_PASSWORD
+// camelCase
+
+//[let const] - ECMAScript 6 [var] not recommended
+// let a;
+// const c = 10;
+// a = true;
+// b = false;
+
+// let a;
+// console.log(a);
+// a = true;
+// console.log(a);
+
+// let myName;
+// myName = "AlexuS";
+// console.log(myName);
+
+// const myName = "AlexuS";
+// myName = "";
+//err 'Assignment to constant variable'

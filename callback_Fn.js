e394bcd22469e1b1bebecfdf1a7010b84c219d2e
Alex <arusskikhqa@gmail.com>
2022-04-18 12:43:54 -0400

    add Callbackfn_ practice

---
+++ callback_Fn.js
@@ -0,0 +1,14 @@
+// function anotherFunction() {
+//   //
+// }
+// function fnWithCallback(callbackFunction) {
+//   callbackFunction();
+// }
+
+// fnWithCallback(anotherFunction);
+
+function printMyName() {
+  console.log("AlexuS");
+}
+
+setTimeout(printMyName, 1000);

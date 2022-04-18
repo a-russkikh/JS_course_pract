80824c23e87df8daa576c65be418e79df76d1f78
Alex <arusskikhqa@gmail.com>
2022-04-18 12:37:10 -0400

    add functions_02 practice

---
+++ functions_ 02.js
@@ -0,0 +1,32 @@
+// function myFn() {} //the shortest function
+
+// myFn();
+
+// const personOne = {
+//   name: "Bob",
+//   age: 21,
+// };
+
+// function increasePersonAge(person) {
+//   person.age += 1;
+//   return person;
+// }
+
+// increasePersonAge(personOne);
+// console.log(personOne.age);
+
+const personOne = {
+  name: "Bob",
+  age: 21,
+};
+
+function increasePersonAge(person) {
+  const updatedPerson = Object.assign({}, person);
+  updatedPerson.age += 1;
+  return updatedPerson;
+}
+
+const updatedPersonOne = increasePersonAge(personOne);
+
+console.log(personOne.age);
+console.log(updatedPersonOne.age);

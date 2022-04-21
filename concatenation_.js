dc7af509c227331ea2d5ba14538b55c9a8fe30d4
Alex <arusskikhqa@gmail.com>
2022-04-21 18:03:58 -0400

    add [200~const hello = "Hello";

---
+++ concatenation_.js
@@ -0,0 +1,16 @@
+// console.log("Hello " + "World");
+
+// const hello = "Hello";
+// const world = "World";
+
+// const greeting = hello + " " + world;
+
+// console.log(greeting);
+
+//Template string literal
+const hello = "Hello";
+const world = "World";
+
+const greeting = `${hello} ${world}`;
+
+console.log(greeting);

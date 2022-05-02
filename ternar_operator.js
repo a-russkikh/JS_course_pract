9b3d5ee968399adbf849083f3f8871ba74194889
Alex <arusskikhqa@gmail.com>
2022-05-02 11:19:22 -0400

    feat:change ternar_operator

---
+++ ternar_operator.js
@@ -0,0 +1,15 @@
+// const value = 11;
+
+// value ? console.log("True") : console.log("False");
+
+// const value1 = 11;
+// const value2 = 25;
+
+// value1 && value2 ? myFunction1(value1, value2) : myFunction2();
+
+let value = 11;
+console.log(value >= 0 ? value : -value);
+
+value = -5;
+const res = value >= 0 ? value : -value;
+console.log(res);

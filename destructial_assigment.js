ac73ba10a9be6bb892a367285af3efb8e71701a5
Alex <arusskikhqa@gmail.com>
2022-05-02 10:36:26 -0400

    feat:add destuctional_assigment

---
+++ destructial_assigment.js
@@ -0,0 +1,10 @@
+const userProfile = {
+  name: "Bogdan",
+  commentsQty: 23,
+  hasSignedAgreement: false,
+};
+const { name, commentsQty } = userProfile;
+const { hasSignedAgreement } = userProfile;
+
+console.log(name);
+console.log(commentsQty);

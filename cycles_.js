1b078415d99f8edf30406cc46e21d466a13ab0ff
Alex <arusskikhqa@gmail.com>
2022-05-02 11:23:33 -0400

    feat:add cycles

---
+++ cycles_.js
@@ -0,0 +1,15 @@
+// const myArray = [true, "abc", 10];
+
+// console.log(myArray[0]);
+// console.log(myArray[1]);
+// console.log(myArray[2]);
+
+const myObject = {
+  x: 10,
+  y: true,
+  z: "abc",
+};
+
+console.log(myObject.x);
+console.log(myObject.y);
+console.log(myObject.z);

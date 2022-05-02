e7b9e5348c8c5c5838fc830cb940ed2a77e615cb
Alex <arusskikhqa@gmail.com>
2022-05-02 10:55:26 -0400

    feat:add if_else_if

---
+++ if_ else_if.js
@@ -0,0 +1,21 @@
+// const age = 25;
+
+// if (age > 18) {
+//   console.log("Is adult");
+// } else if (age >= 12) {
+//   console.log("Is teenager");
+// } else {
+//   console.log("Is child");
+// }
+
+const age = 17;
+
+if (age > 18) {
+  console.log("Is adult");
+}
+if (age >= 12 && age < 18) {
+  console.log("Is teenager");
+}
+if (age < 12) {
+  console.log("Is child");
+}

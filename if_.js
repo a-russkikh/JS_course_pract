aeb735201c51b4326437efa002a5f2dcae077bcc
Alex <arusskikhqa@gmail.com>
2022-05-02 10:46:26 -0400

    feat:change if_

--- if_.js
+++ if_.js
@@ -1,1 +1,17 @@
 // if(consition)
+
+// let val = 10;
+
+// if (val > 5) {
+//   val += 20;
+// }
+
+// console.log(val);
+
+const person = {
+  age: 20,
+};
+
+if (!person.name) {
+  console.log("Name is not defined");
+}

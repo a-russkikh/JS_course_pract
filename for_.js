fe18fec66b87e41d41af91c8e0b3b15ffb0c9715
Alex <arusskikhqa@gmail.com>
2022-05-02 11:34:09 -0400

    feat:add for_

---
+++ for_.js
@@ -0,0 +1,31 @@
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// const myArray = ["first", "second", "third"];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// Method FOREACH
+
+const myArray1 = ["first", "second", "third"];
+
+myArray1.forEach((element, index) => {
+  console.log(element, index);
+});
